545124c1335965023723381fc56760489724fba0
Dave Dawson <dave@iamdavedawson.com>
2021-08-31 16:07:47 -0700

    Remove double footer content

--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -41,18 +41,6 @@ const Letter = () => (
           </a>
           .
         </p>
-        <div className="page-footer">
-          <p className="small">
-            <em>Case studies available upon request.</em>
-          </p>
-          <p className="small">
-            Find me on <a href="https://twitter.com/davedawson">Twitter</a> and
-            <a href="https://www.linkedin.com/in/dave-dawson-b1127813b/">
-              LinkedIn
-            </a>
-            .
-          </p>
-        </div>
       </div>
     </div>
   </div>
